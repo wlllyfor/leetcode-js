32f94e0eea53252cdf66f9639e1ff82452bd9042
wlllyfor <958167224@qq.com>
2022-03-27 19:37:46 +0800

    feat: three sum add notes

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -3,9 +3,11 @@
  *
  * [15] 三数之和
  * 
+ * 如果数组长度小于3，就直接返回 []
+ * 
  * [-1,0,1,2,-1,-4]
  * 
- * 首先，给数组排序
+ * 给数组排序
  * 
  * [-4,-1,-1,0,1,2]
  * 
@@ -16,14 +18,18 @@
  * [-4,-1,-1,0,1,2,9]
  * 
  * nums[i] + nums[l] + nums[r] 和 0 作比较
- * 
  * nums[i] + nums[l] + nums[r] > 0，说明 r 大了，k 左移
- * 
  * nums[i] + nums[l] + nums[r] < 0，说明 l 小了，j 右移
- * 
  * nums[i] + nums[l] + nums[r] = 0，找到，记录下来
  * 
- * 遍历到 nums.length - 2 就行
+ * 注意要去重
+ * 
+ *  i   l                r
+ * [-1, 0,0,0,0,0,1,1,1,1]
+ * 
+ * 遇到 nums[l] === nums[l+1] l++
+ * 遇到 nums[r] === nums[r-1] r++
+ * 
  */
 
 // @lc code=start
@@ -32,15 +38,14 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  
-  const res = []
-  const len = nums.length
-  if(len < 3) return res
-
+  let res = []
+  let len = nums.length
+  if (len < 3) {
+    return res
+  }
   nums.sort((a, b) => a - b)
 
   for (let i = 0; i < len; i++) {
-
     if (nums[i] > 0) {
       break
     }
@@ -48,31 +53,34 @@ var threeSum = function(nums) {
     if (nums[i] === nums[i - 1]) {
       continue
     }
-
-    let l = i + 1
-    let r = len - 1 
     
+    let l = i + 1
+    let r = len - 1
+
     while (l < r) {
       const sum = nums[i] + nums[l] + nums[r]
-
       if (sum === 0) {
+        res.push([nums[i], nums[l], nums[r]])
         while(nums[l] === nums[l + 1]) {
           l++
         }
         while(nums[r] === nums[r - 1]) {
           r--
         }
-        res.push([nums[i], nums[l], nums[r]])
         r--
-      } else if (sum > 0) {
+      }
+      if (sum > 0) {
         r--
-      } else if (sum < 0) {
+      }
+      if (sum < 0) {
         l++
       }
+      
     }
-
   }
+
   return res
+  
 };
 // @lc code=end
 
