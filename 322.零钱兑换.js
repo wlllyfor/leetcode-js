cf058e98a677aa1f1023192fadbac7980f29cdf9
wlllyfor <958167224@qq.com>
2022-04-01 16:03:15 +0800

    feat：add 322

--- 322.零钱兑换.js
+++ 322.零钱兑换.js
@@ -6,13 +6,18 @@
  * 
  * 每次先兑换当前能兑换的最大零钱
  * 
- * [5,4,3] 7
+ * [1, 2, 5] 11
  * 
- * 4 + 3 = 7
+ * f(0) = 0
+ * f(1) = Math.min(f(1-1), f(1-2), f(1-5)) + 1
+ * f(2) = Math.min(f(2-1), f(2-2), f(2-5)) + 1
+ * f(3) = Math.min(f(3-1), f(3-2), f(3-5)) + 1
  * 
- * [2,1] 2
+ * const dp = []
  * 
- *  11
+ * dp[i] = Math.min(dp[i], dp[i-conins[j]] + 1)
+ * 
+ * [2] 3
  */
 
 // @lc code=start
@@ -22,31 +27,20 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-  coins.sort((a, b) => b - a)
-  let count = Number.MAX_VALUE
-  for (let i = 0; i < coins.length; i++) {
-    count = Math.min(count, find(i))
-  }
-
-  function find(i) {
-    let innerCount = 0
-    let innerAmount = amount
-    while(innerAmount) {
-      const innerCoins = coins.slice(i)
-      let max = innerCoins[0]
-      if(innerAmount >= max) {
-        innerAmount -= max
-        innerCount++
-      } else {
-        innerCoins.shift()
-        if (innerCoins.length === 0 && innerAmount > 0) {
-          return Number.MAX_VALUE
-        }
+  const dp = []
+  dp[0] = 0
+  for (let i = 1; i <= amount; i++) {
+    dp[i] = Number.MAX_SAFE_INTEGER
+    for (let j = 0; j < coins.length; j++) {
+      if (i - coins[j] >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1)
       }
     }
-    return innerCount
   }
-  return count
+  if (dp[amount] === Number.MAX_SAFE_INTEGER) {
+    return -1
+  }
+  return dp[amount]
 };
 // @lc code=end
 
