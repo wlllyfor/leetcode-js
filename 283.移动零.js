8a38da6ab38dc973fb8904f8e6c85a1a958c4389
wlllyfor <958167224@qq.com>
2022-04-02 18:17:45 +0800

    feat：add 283

---
+++ 283.移动零.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ * 
+ *        i
+ *          j
+ * [0,1,0,3,12]
+ * [1,3,12 ]
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let i = 0
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== 0) {
+      nums[i] = nums[j]
+      i++
+    }
+  }
+  for (let k = i; k < nums.length; k++) {
+    nums[k] = 0
+  }
+  return nums
+};
+// @lc code=end
+
