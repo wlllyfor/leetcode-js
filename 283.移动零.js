ee1df41203f5f079d4247b8f4cd13479a415a41b
wlllyfor <958167224@qq.com>
2022-04-02 18:27:40 +0800

    feat：update 283

--- 283.移动零.js
+++ 283.移动零.js
@@ -3,12 +3,10 @@
  *
  * [283] 移动零
  * 
- *        i
+ *      i
  *          j
  * [0,1,0,3,12]
- * [1,3,12 ]
- * 
- * 
+ * [1,3,12,0,0]
  * 
  */
 
@@ -17,18 +15,29 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
+// var moveZeroes = function(nums) {
+//   let i = 0
+//   for (let j = 0; j < nums.length; j++) {
+//     if (nums[j] !== 0) {
+//       nums[i] = nums[j]
+//       i++
+//     }
+//   }
+//   for (let k = i; k < nums.length; k++) {
+//     nums[k] = 0
+//   }
+//   return nums
+// };
+
+function moveZeroes(nums) {
   let i = 0
   for (let j = 0; j < nums.length; j++) {
     if (nums[j] !== 0) {
-      nums[i] = nums[j]
+      [nums[i], nums[j]] = [nums[j], nums[i]]
       i++
     }
   }
-  for (let k = i; k < nums.length; k++) {
-    nums[k] = 0
-  }
   return nums
-};
+}
 // @lc code=end
 
