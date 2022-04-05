9b5102d9e63cb216d5c48196d2890e5c2e4e97c7
wlllyfor <958167224@qq.com>
2022-04-05 11:50:35 +0800

    update：update 209

--- 209.长度最小的子数组.js
+++ 209.长度最小的子数组.js
@@ -16,33 +16,60 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSubArrayLen = function(target, nums) {
-  let slow = 0, fast = 0
-  let sum = 0
-  let min = Infinity
-  while (slow <= nums.length - 1) {
-    sum = nums[slow]
+// var minSubArrayLen = function(target, nums) {
+//   let slow = 0, fast = 0
+//   let sum = 0
+//   let min = Infinity
+//   while (slow <= nums.length - 1) {
+//     sum = nums[slow]
     
-    while (sum < target) {
-      if (fast === nums.length - 1) {
-        break
-      }
-      fast++
-      sum += nums[fast]
-    }
+//     while (sum < target) {
+//       if (fast === nums.length - 1) {
+//         break
+//       }
+//       fast++
+//       sum += nums[fast]
+//     }
 
-    if (sum >= target) {
-      min = Math.min(fast - slow + 1, min)
-      slow++
-      fast = slow
+//     if (sum >= target) {
+//       min = Math.min(fast - slow + 1, min)
+//       slow++
+//       fast = slow
       
-    } else {
-      console.log(sum)
-      min = min === Infinity ? 0 : min
-      break
+//     } else {
+//       console.log(sum)
+//       min = min === Infinity ? 0 : min
+//       break
+//     }
+//   }
+//   return min
+// };
+
+/**
+ * 
+ *            i
+ *            j
+ * [2,3,1,2,4,3]
+ * 
+ *  i
+ *          j
+ * [1,2,3,4,5] 15
+ */
+function minSubArrayLen (target, nums) {
+  let slow = 0, fast = 0
+  let len = nums.length
+  let min = len + 1
+  let sum = 0
+  while (fast < len) {
+    sum += nums[fast]
+    while (sum >= target) {
+      min = Math.min(min, fast - slow + 1)
+      sum -= nums[slow]
+      slow++
     }
+    fast++
   }
-  return min
-};
+  return min === len - 1 ? 0 : min
+}
 // @lc code=end
 
