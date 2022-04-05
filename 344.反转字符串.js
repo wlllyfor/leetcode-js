0d3f4edcc5d40e28da244de82d8cfcd8296c01b2
wlllyfor <958167224@qq.com>
2022-04-05 10:02:11 +0800

    feat：add 344

---
+++ 344.反转字符串.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=javascript
+ *
+ * [344] 反转字符串
+ * 
+ *   i               j
+ * ["h","e","l","l","o"]
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  let left = 0, right = s.length - 1
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]]
+    left++
+    right--
+  }
+  return s
+};
+// @lc code=end
+
