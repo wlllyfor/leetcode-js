a6ddc83faa9a0ceeb3d4979bfc0b0475219d761c
wlllyfor <958167224@qq.com>
2022-04-06 11:43:03 +0800

    feat：add 876

---
+++ 876.链表的中间结点.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=javascript
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * 
+ * [1,2,3,4,5]
+ * 1 1
+ * 2 2
+ * 3 2
+ * 4 3
+ */
+
+function getLength(head) {
+  let len = 0
+  while(head) {
+    head = head.next
+    len++
+  }
+  return len
+}
+var middleNode = function(head) {
+  let len = getLength(head)
+  let mid = Math.floor(len / 2) + 1
+  let zero = {
+    next: head
+  }
+  while(mid) {
+    zero = zero.next
+    mid--
+  }
+  return zero
+};
+// @lc code=end
+
