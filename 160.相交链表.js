e72ed123812ad6eea55edf9f3d8fac3e40bdde0a
wlllyfor <958167224@qq.com>
2022-04-06 14:17:34 +0800

    feat: add 160

---
+++ 160.相交链表.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ * 
+ */
+function getLength (head) {
+  let len = 0
+  while(head) {
+    head = head.next
+    len++
+  }
+  return len
+}
+var getIntersectionNode = function(headA, headB) {
+    let aLen = getLength(headA)
+    let bLen = getLength(headB)
+    let longerHead = aLen > bLen ? headA: headB
+    let shorterHead =  aLen > bLen ? headB: headA
+
+    let gap = aLen > bLen ? aLen - bLen : bLen - aLen
+
+    while(gap) {
+      longerHead = longerHead.next
+      gap--
+    }
+    let zero1 = {
+      next: longerHead
+    }
+    let zero2 = {
+      next: shorterHead
+    }
+    while(zero1) {
+      if (zero1.next === zero2.next) {
+        return zero1.next
+      }
+      zero1 = zero1.next
+      zero2 = zero2.next
+    }
+    return null
+};
+// @lc code=end
+
