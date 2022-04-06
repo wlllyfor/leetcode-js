ac12765dbe3d9b49a1cfbd43c6977f0e41b11a55
wlllyfor <958167224@qq.com>
2022-04-06 11:17:43 +0800

    feat：add 21

---
+++ 21.合并两个有序链表.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  let zero = {
+    next: new ListNode()
+  }
+  let tmp = zero
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      tmp.next = list1
+      list1 = list1.next
+    } else {
+      tmp.next = list2
+      list2 = list2.next
+    }
+    tmp = tmp.next
+  }
+  tmp.next = list1 ? list1 : list2
+  return zero.next
+};
+// @lc code=end
+
