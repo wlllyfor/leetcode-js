37cc88c1a37cf7201b20fc78879f77133380bcd2
wlllyfor <958167224@qq.com>
2022-04-07 15:06:19 +0800

    feat：update 234

--- 234.回文链表.js
+++ 234.回文链表.js
@@ -15,22 +15,65 @@
 /**
  * @param {ListNode} head
  * @return {boolean}
+ * 
+ *  i     j
+ * [1,2,2,1]
  */
-var isPalindrome = function(head) {
-  let arr = []
-  while(head) {
-    arr.push(head.val)
-    head = head.next
+// var isPalindrome = function(head) {
+//   let arr = []
+//   while(head) {
+//     arr.push(head.val)
+//     head = head.next
+//   }
+//   let l = 0, r = arr.length - 1
+//   while(l < r) {
+//     if (arr[l] !== arr[r]) {
+//       return false
+//     }
+//     l++
+//     r--
+//   }
+//   return true
+// };
+
+// function isPalindrome(head) {
+//   let str = ''
+//   while(head) {
+//     str += head.val
+//     head = head.next
+//   }
+//   console.log(str)
+//   return str.split('').reverse().join('') === str
+// }
+
+function isPalindrome(head) {
+  let pre = null
+  let fast = slow = head
+
+  while(fast && fast.next) {
+    fast = fast.next.next
+    let next = slow.next
+    slow.next = pre
+    pre = slow
+    slow = next
   }
-  let l = 0, r = arr.length - 1
-  while(l < r) {
-    if (arr[l] !== arr[r]) {
+
+  console.log(pre)
+  console.log(slow)
+
+  if (fast) {
+    slow = slow.next
+  }
+
+  while(pre && slow) {
+    if (pre.val !== slow.val) {
       return false
     }
-    l++
-    r--
+    pre = pre.next
+    slow = slow.next
   }
   return true
-};
+  
+}
 // @lc code=end
 
