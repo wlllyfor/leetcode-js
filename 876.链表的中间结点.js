1d60f43972400b2ed982c4275f5e6460a2d7d4eb
wlllyfor <958167224@qq.com>
2022-04-07 13:43:48 +0800

    feat：update 876

--- 876.链表的中间结点.js
+++ 876.链表的中间结点.js
@@ -23,26 +23,35 @@
  * 4 3
  */
 
-function getLength(head) {
-  let len = 0
-  while(head) {
-    head = head.next
-    len++
+// function getLength(head) {
+//   let len = 0
+//   while(head) {
+//     head = head.next
+//     len++
+//   }
+//   return len
+// }
+// var middleNode = function(head) {
+//   let len = getLength(head)
+//   let mid = Math.floor(len / 2) + 1
+//   let zero = {
+//     next: head
+//   }
+//   while(mid) {
+//     zero = zero.next
+//     mid--
+//   }
+//   return zero
+// };
+
+function middleNode (head) {
+  let fast = slow = head
+  while(fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
   }
-  return len
+  return slow
 }
-var middleNode = function(head) {
-  let len = getLength(head)
-  let mid = Math.floor(len / 2) + 1
-  let zero = {
-    next: head
-  }
-  while(mid) {
-    zero = zero.next
-    mid--
-  }
-  return zero
-};
 
 // @lc code=end
 
