d9a57c49c112066a02b5161dd159a052f63f77c4
wlllyfor <958167224@qq.com>
2022-04-10 07:59:53 +0800

    feat：update 144

--- 144.二叉树的前序遍历.js
+++ 144.二叉树的前序遍历.js
@@ -50,21 +50,46 @@
  * 124536
  * 中左右
  */
-function preorderTraversal(root) {
+// function preorderTraversal(root) {
+//   let res = []
+//   if (!root) {
+//     return res
+//   }
+//   let stack = []
+
+//   while (root || stack.length) {
+//     while(root) {
+//       res.push(root.val)
+//       stack.push(root)
+//       root = root.left
+//     }
+//     root = stack.pop()
+//     root = root.right
+//   }
+//   return res
+// }
+
+/**
+ *          1
+ *       2    3
+ *     4  5  6
+ * 
+ * 124536
+ * 
+ * stack: [6]
+ * res: [1,2,4,5,3]
+ */
+function preorderTraversal (root) {
   let res = []
   if (!root) {
     return res
   }
-  let stack = []
-
-  while (root || stack.length) {
-    while(root) {
-      res.push(root.val)
-      stack.push(root)
-      root = root.left
-    }
-    root = stack.pop()
-    root = root.right
+  let stack = [root]
+  while(stack.length) {
+    const node = stack.pop()
+    res.push(node.val)
+    node.right && stack.push(node.right)
+    node.left && stack.push(node.left)
   }
   return res
 }
