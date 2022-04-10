be0abe58d5045a61e8f55e702788f286689fc8d3
wlllyfor <958167224@qq.com>
2022-04-10 15:46:14 +0800

    feat：update 111

--- 111.二叉树的最小深度.js
+++ 111.二叉树的最小深度.js
@@ -20,13 +20,14 @@
 var minDepth = function(root) {
   if (!root) {
     return 0
-  }else if (!root.left) {
-    return minDepth(root.right) + 1
-  }else if (!root.right) {
-    return minDepth(root.left) + 1
-  } else {
-    return Math.min(minDepth(root.left), minDepth(root.right)) + 1
   }
+  if (!root.left) {
+    return minDepth(root.right) + 1
+  }
+  if (!root.right) {
+    return minDepth(root.left) + 1
+  }
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1
 };
 // @lc code=end
 
