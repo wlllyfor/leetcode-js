49a16ac10e82c236e5a8ae93ac55db72917e22a0
wlllyfor <958167224@qq.com>
2022-04-11 15:36:44 +0800

    update 617

--- 617.合并二叉树.js
+++ 617.合并二叉树.js
@@ -18,19 +18,35 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
-var mergeTrees = function(root1, root2) {
-  if (!root1) {
-    return root2
-  }
-  if (!root2) {
+// var mergeTrees = function(root1, root2) {
+//   if (!root1) {
+//     return root2
+//   }
+//   if (!root2) {
+//     return root1
+//   }
+//   console.log('root1', root1)
+//   console.log('root2', root2)
+//   let merged = new TreeNode(root1.val + root2.val)
+//   merged.left = mergeTrees(root1.left, root2.left)
+//   merged.right = mergeTrees(root1.right, root2.right)
+//   return merged
+// };
+
+function mergeTrees (root1, root2) {
+  function travel(root1, root2) {
+    if (!root1) {
+      return root2
+    }
+    if (!root2) {
+      return root1
+    }
+    root1.val += root2.val
+    root1.left = travel(root1.left, root2.left)
+    root1.right = travel(root1.right, root2.right)
     return root1
   }
-  console.log('root1', root1)
-  console.log('root2', root2)
-  let merged = new TreeNode(root1.val + root2.val)
-  merged.left = mergeTrees(root1.left, root2.left)
-  merged.right = mergeTrees(root1.right, root2.right)
-  return merged
-};
+  return travel(root1, root2)
+}
 // @lc code=end
 
