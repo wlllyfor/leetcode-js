6a62304c44c9e44fe6214a201ff1a11c367b6b09
wlllyfor <958167224@qq.com>
2022-04-13 16:44:31 +0800

    feat：add 107

---
+++ 107.二叉树的层序遍历-ii.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=javascript
+ *
+ * [107] 二叉树的层序遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  const res = []
+  if (!root) {
+    return res
+  }
+  const queue = [root]
+  while (queue.length) {
+    let len = queue.length
+    let curLevel = []
+    while(len--) {
+      const node = queue.shift()
+      curLevel.push(node.val)
+      node.left && queue.push(node.left)
+      node.right && queue.push(node.right)
+    }
+    res.unshift(curLevel)
+  }
+  return res
+};
+// @lc code=end
+
