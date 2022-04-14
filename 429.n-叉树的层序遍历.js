56fcbe1c4f88e7fb4daeb21eabc4c9650d4a4557
wlllyfor <958167224@qq.com>
2022-04-14 11:55:18 +0800

    feat：add 429

---
+++ 429.n-叉树的层序遍历.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=429 lang=javascript
+ *
+ * [429] N 叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  let res = []
+  if (!root) {
+    return res
+  }
+  let queue = [root]
+  while(queue.length) {
+    let len = queue.length
+    let curLevel = []
+    while(len--) {
+      let node = queue.shift()
+      curLevel.push(node.val)
+      for (let child of node.children) {
+        queue.push(child)
+      }
+    }
+    res.push(curLevel)
+  }
+  return res
+};
+// @lc code=end
+
