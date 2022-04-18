7a973274ea1cb2d550ae66c09f0d50dfae686d82
wlllyfor <958167224@qq.com>
2022-04-19 06:26:52 +0800

    add：230

---
+++ 230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+// var kthSmallest = function(root, k) {
+//   let arr = []
+//   function travel(node) {
+//     if (!node) {
+//       return
+//     }
+//     travel(node.left)
+//     arr.push(node.val)
+//     travel(node.right)
+//   }
+//   travel(root)
+//   return arr[k - 1]
+// };
+
+function kthSmallest (root, k) {
+  let count = 0
+  let res
+  function travel(node) {
+    if (!node) {
+      return null
+    }
+    travel(node.left)
+    count++
+    if (count === k) {
+      res = node.val
+    }
+    travel(node.right)
+  }
+  travel(root)
+  return res
+}
+// @lc code=end
+
