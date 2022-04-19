c45bcd303502f55ff5ccdd97f90fbdd7ccd18b46
wlllyfor <958167224@qq.com>
2022-04-19 18:09:51 +0800

    feat：add 20

---
+++ 20.有效的括号.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ * 
+ * s ')'
+ * stack [(]
+ */
+var isValid = function(s) {
+  const obj = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+  let arr = s.split('')
+  let stack = []
+  while(arr.length) {
+    let item = arr.shift()
+    if (obj[stack[stack.length - 1]] === item) {
+      stack.pop()
+    } else {
+      stack.push(item)
+    }
+  }
+  return stack.length === 0
+};
+// @lc code=end
+
