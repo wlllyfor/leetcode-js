7c038cd522c3a733d45369d01298cc597a6fbb40
wlllyfor <958167224@qq.com>
2022-04-21 08:38:40 +0800

    feat：add 704

---
+++ 704.二分查找.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ * 
+ * [-1,0,3,5,9,12]
+ * 
+ * mid 2
+ * left 2
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  if (right === 0 && nums[0] === target) {
+    return 0
+  }
+  while(left <= right) {
+    let mid = (left + right) >> 1
+    if (nums[mid] === target) {
+      return mid
+    }
+    if (nums[mid] > target) {
+      right = mid - 1
+    }
+    if (nums[mid] < target) {
+      left = mid + 1
+    }
+  }
+  return -1
+};
+// @lc code=end
+
