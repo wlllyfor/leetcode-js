093e7c76d494c132783c5b734cd7b413b1587840
wlllyfor <958167224@qq.com>
2022-04-23 21:56:54 +0800

    feat：add 39

--- 39.组合总和.js
+++ 39.组合总和.js
@@ -11,7 +11,30 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-
+  let res = []
+  let path = []
+  candidates.sort()
+  backTrack(0, 0)
+  return res
+  function backTrack (start, sum) {
+    if (sum > target) {
+      return 
+    }
+    if (sum === target) {
+      res.push([...path])
+    }
+    for (let i = start; i < candidates.length; i++) {
+      let num = candidates[i] 
+      if (num + sum > target) {
+        continue
+      }
+      path.push(num)
+      sum += num
+      backTrack(i, sum)
+      path.pop()
+      sum -= num
+    }
+  }
 };
 // @lc code=end
 
