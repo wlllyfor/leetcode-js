7b6bf2b8d6974752818f0b3471d8199e99018506
wlllyfor <958167224@qq.com>
2022-04-23 10:52:19 +0800

    feat：update 150

--- 150.逆波兰表达式求值.js
+++ 150.逆波兰表达式求值.js
@@ -10,35 +10,55 @@
  * @return {number}
  */
 
-function calc(val1, val2, s) {
-  if (s === '+') {
-    return val1 + val2
-  }
-  if (s === '-') {
-    return val1 - val2
-  }
-  if (s === '*') {
-    return val1 * val2
-  }
-  if (s === '/') {
-    return parseInt(val1 / val2)
-  }
-}
+// function calc(val1, val2, s) {
+//   switch(s) {
+//     case '+':
+//     return val1 + val2
+//     case '-':
+//     return val1 - val2
+//     case '*':
+//     return val1 * val2
+//     case '/':
+//     return (val1 / val2) | 0
+//   }
+// }
 
-var evalRPN = function(tokens) {
+// var evalRPN = function(tokens) {
   
+//   let stack = []
+//   for (let item of tokens) {
+//     if (item>= -200 && item <= 200) {
+//       stack.push(Number(item))
+//     } else {
+//       const val2 = stack.pop()
+//       const val1 = stack.pop()
+//       const calcRes = calc(val1, val2, item)
+//       stack.push(calcRes)
+//     }
+//   }
+//   return stack[0]
+// };
+
+// 策略模式
+function evalRPN (tokens) {
+  let calc = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => (a / b) | 0,
+  }
   let stack = []
   for (let item of tokens) {
-    if (item>= -200 && item <= 200) {
-      stack.push(Number(item))
-    } else {
+    if (item in calc) {
       const val2 = stack.pop()
       const val1 = stack.pop()
-      const clacRes = calc(val1, val2, item)
-      stack.push(clacRes)
+      const calcRes = calc[item](val1, val2)
+      stack.push(calcRes)
+    } else {
+      stack.push(Number(item))
     }
   }
-  return stack[0]
-};
+  return stack.pop()
+}
 // @lc code=end
 
