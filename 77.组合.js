4cdd6501519513d3099a9ed274471b4f866e67cf
wlllyfor <958167224@qq.com>
2022-04-26 06:09:31 +0800

    feat：update 77

--- 77.组合.js
+++ 77.组合.js
@@ -13,7 +13,7 @@
 
 /**
  * 
- * n - (k - len) + 1
+ * 
  */
 // function combine(n, k) {
 //   let path = []
@@ -37,26 +37,54 @@
 //   }
 // }
 
+
+/**
+ * 
+ * 剪枝优化
+ * n = 4
+ * k = 4
+ * n - (k - len) + 1
+ * n - k + len + 1
+ * 
+ * len 0  i <= 1
+ * len 1  i <= 2
+ * len 2  i <= 3
+ * len 3  i <= 4
+ * 
+ * 
+ * 
+ * n = 4
+ * k = 2
+ * n - k + len + 1
+ * 
+ * len 0  i <= 3
+ * len 1  i <= 4
+ * 
+ */
 function combine(n, k) {
-  let path = []
   let res = []
+  let path = []
 
   backTrack(1)
 
   return res
 
-  function backTrack(cur) {
+  function backTrack (cur) {
+    console.log('cur :>> ', cur);
     let len = path.length
     if (len === k) {
       res.push([...path])
       return
     }
+
     for (let i = cur; i <= n - k + len + 1; i++) {
       path.push(i)
       backTrack(i + 1)
       path.pop()
     }
   }
+
+  
 }
 // @lc code=end
 
