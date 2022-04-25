037d704e09c8805866c8d0634cdc4b016202928b
wlllyfor <958167224@qq.com>
2022-04-26 06:39:18 +0800

    feat：add 78

--- 78.子集.js
+++ 78.子集.js
@@ -8,9 +8,29 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
+ * 
+ * path
  */
 var subsets = function(nums) {
+  let res = []
+  let path = []
 
+  backTrack(0)
+
+  return res
+
+  function backTrack(cur) {
+    res.push([...path])
+    if (cur === nums.length) {
+      return
+    }
+
+    for (let i = cur; i < nums.length; i++) {
+      path.push(nums[i])
+      backTrack(i + 1)
+      path.pop()
+    }
+  }
 };
 // @lc code=end
 
