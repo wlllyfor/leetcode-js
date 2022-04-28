9a6a919e3df509126941dc21a59a25cf77709e44
wlllyfor <958167224@qq.com>
2022-04-28 23:26:50 +0800

    feat：add 198

---
+++ 198.打家劫舍.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * [2,7,3,1,9]
+ */
+
+/**
+ * dp[0] = 0
+ * dp[1] = nums[0]
+ * dp[2] = Math.max(dp[0] + nums[2 - 1], dp[1])
+ *         
+ * dp[3] = Math.max(dp[1] + nums[3 - 1], dp[2])
+ * 
+ * dp[i] = Math.max(dp[i-2]+ nums[i - 1], dp[i-1])
+ * 
+ * [1]
+ * 
+ * [1, 2]
+ */
+var rob = function(nums) {
+  let dp = [0]
+  let n = nums.length
+  dp[1] = nums[0]
+  if (n >= 2) {
+    for (let i = 2; i <= n; i++) {
+      dp[i] = Math.max(dp[i-2]+ nums[i - 1], dp[i-1])
+    }
+  }
+
+  return dp[n]
+};
+// @lc code=end
+
