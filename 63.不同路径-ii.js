9748c007b919be68922c522849dd1513b04c3caa
wlllyfor <958167224@qq.com>
2022-04-28 20:43:35 +0800

    add 63

---
+++ 63.不同路径-ii.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=javascript
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+  let m = obstacleGrid.length
+  let n = obstacleGrid[0].length
+
+  let dp = new Array(m).fill(0).map(() => {
+    return new Array(n).fill(0)
+  })
+
+  for(let i=0; i<m && obstacleGrid[i][0]===0; i++){
+    dp[i][0] = 1
+  }
+  for(let i=0; i<n && obstacleGrid[0][i]===0; i++){
+    dp[0][i] = 1
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; i < n; j++) {
+      if (obstacleGrid[i][j] === 1 ) {
+        dp[i][j] = 0
+      } else {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+      }
+    }
+  }
+  return dp[m - 1][n - 1]
+};
+// @lc code=end
+
