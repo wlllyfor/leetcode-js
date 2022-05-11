fcc8d09255b3cd5285507606b67130bc37c5415c
wlllyfor <958167224@qq.com>
2022-05-11 21:24:05 +0800

    update

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -15,27 +15,87 @@
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
+ * 
+ *              i
+ * [0,1,3,0,4,2,4,2]
+ *                  j
+ * 
  */
-// var removeElement = function(nums, val) {
-//   let slow = 0, fast = 0
-//   const len = nums.length
-//   while(fast < len) {
-//     if (nums[fast] !== val) {
-//       nums[slow] = nums[fast]
-//       slow++
-//     }
-//     fast++
-//   }
-//   return slow
-// };
+function removeElement (nums, val) {
+  let slow = 0, fast = 0
+  const len = nums.length
+  while(fast < len) {
+    if (nums[fast] !== val) {
+      nums[slow] = nums[fast]
+      slow++
+    }
+    fast++
+  }
+  return slow
+};
 
 /**
- *          l         
- *          r
- * [1,5,2,3,4,5,1,1]  1
- * [5,5,2,3,4]
  * 
- * [1]
+ *  l         
+ * [3,2,2,3]  
+ *        r
+ * 
+ *  l         
+ * [3,2,2,3]  
+ *      r
+ * 
+ *  l         
+ * [2,2,2,3]  
+ *      r
+ * 
+ *    l         
+ * [2,2,2,3]  
+ *      r
+ * 
+ * 3
+ * 
+ * 
+ *  l
+ * [0,1,2,2,3,0,4,2]
+ *                r
+ * 
+ *    l
+ * [0,1,2,2,3,0,4,2]
+ *                r
+ * 
+ *      l
+ * [0,1,2,2,3,0,4,2]
+ *                r
+ * 
+ *      l
+ * [0,1,2,2,3,0,4,2]
+ *              r
+ * 
+ *      l
+ * [0,1,4,2,3,0,4,2]
+ *              r
+ * 
+ *        l
+ * [0,1,4,2,3,0,4,2]
+ *              r
+ * 
+ *        l
+ * [0,1,4,4,3,0,4,2]
+ *              r
+ * 
+ *          l
+ * [0,1,4,4,3,0,4,2]
+ *              r
+ * 
+ *            l
+ * [0,1,4,4,3,0,4,2]
+ *              r
+ * 
+ *              l
+ * [0,1,4,4,3,0,4,2]
+ *              r
+ * 
+ * 2
  */
 
 function removeElement (nums, val) {
