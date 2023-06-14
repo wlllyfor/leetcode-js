55f50c7e1219dc3bc5d41ec8a94ecdbea172c8b5
wlllyfor <958167224@qq.com>
2023-06-14 11:34:47 +0800

    feat: add html

---
+++ test/1.js
@@ -0,0 +1,211 @@
+const a = {
+  Response: {
+    Data: {
+      root: {
+        ID: -1,
+        Pid: -1,
+        Name: 'root',
+        Remark: '根节点',
+        children: [
+          {
+            ID: 1,
+            Pid: -1,
+            Name: '公有云-供测试',
+            Remark: '私有云目录类型test1047',
+            State: 1,
+            children: [
+              {
+                ID: 6,
+                Pid: 1,
+                Name: '服务诉求',
+                Remark: '服务诉求',
+                State: 1,
+                children: [
+                  {
+                    ID: 7,
+                    Pid: 6,
+                    Name: '专家服务',
+                    Remark: '专家服务',
+                    State: 1,
+                    children: [
+                      { ID: 21, Pid: 7, Name: '高可用建设', State: 1 },
+                    ],
+                  },
+                  {
+                    ID: 8,
+                    Pid: 6,
+                    Name: '三级目录2',
+                    Remark: '重复性测试',
+                    State: 1,
+                    children: [
+                      { ID: 22, Pid: 8, Name: '子目录345435', State: 1 },
+                      { ID: 43, Pid: 8, Name: '子目录', State: 1 },
+                    ],
+                  },
+                  {
+                    ID: 10,
+                    Pid: 6,
+                    Name: '高级服务',
+                    Remark: '高级服务类型',
+                    State: 1,
+                    children: [{ ID: 41, Pid: 10, Name: '赋能交流', State: 1 }],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            ID: 3,
+            Pid: -1,
+            Name: '公有云233',
+            Remark: '公有云目录类型',
+            State: 1,
+            children: [
+              {
+                ID: 62,
+                Pid: 3,
+                Name: '服务诉求',
+                Remark: '服务诉求',
+                State: 1,
+                children: [
+                  {
+                    ID: 63,
+                    Pid: 62,
+                    Name: '专家服务',
+                    Remark: '专家服务',
+                    State: 1,
+                    children: [
+                      {
+                        ID: 64,
+                        Pid: 63,
+                        Name: '架构优化',
+                        Remark: '架构优化',
+                        State: 1,
+                      },
+                      {
+                        ID: 65,
+                        Pid: 63,
+                        Name: '稳定性治理',
+                        Remark: '稳定性治理',
+                        State: 1,
+                      },
+                      {
+                        ID: 66,
+                        Pid: 63,
+                        Name: '高可用建设',
+                        Remark: '高可用建设',
+                        State: 1,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            ID: 11,
+            Pid: -1,
+            Name: '其他诉求',
+            Remark: '其他诉求类型',
+            State: 1,
+            children: [
+              {
+                ID: 12,
+                Pid: 11,
+                Name: '商务相关',
+                Remark: '商务相关类型',
+                State: 1,
+                children: [
+                  {
+                    ID: 13,
+                    Pid: 12,
+                    Name: '投诉建议',
+                    Remark: '投诉建议类型111',
+                    State: 1,
+                    children: [
+                      {
+                        ID: 14,
+                        Pid: 13,
+                        Name: '服务质量投诉',
+                        Remark: '服务质量投诉类型',
+                        State: 1,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            ID: 17,
+            Pid: -1,
+            Name: '私有云123',
+            Remark: '私有云目录类型',
+            State: 1,
+            children: [
+              {
+                ID: 24,
+                Pid: 17,
+                Name: '私有云111-第二级234234234',
+                Remark: '私有云目录类型',
+                State: 1,
+                children: [
+                  {
+                    ID: 25,
+                    Pid: 24,
+                    Name: '私有云111-第三级',
+                    Remark: '私有云目录类型',
+                    State: 1,
+                    children: [
+                      {
+                        ID: 26,
+                        Pid: 25,
+                        Name: '私有云111-第四级',
+                        Remark: '私有云目录类型',
+                        State: 1,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            ID: 47,
+            Pid: -1,
+            Name: '私有云测试',
+            State: 1,
+            children: [
+              {
+                ID: 50,
+                Pid: 47,
+                Name: '私有云二级目录s2',
+                State: 1,
+                children: [
+                  {
+                    ID: 51,
+                    Pid: 50,
+                    Name: '私有云三级目录t1',
+                    State: 1,
+                    children: [
+                      { ID: 55, Pid: 51, Name: '私有云四级目录f1', State: 1 },
+                      {
+                        ID: 56,
+                        Pid: 51,
+                        Name: '私有云四级目录f2',
+                        Remark: 'cesdsssssss',
+                        State: 1,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    },
+    Status: 0,
+    RequestId: '6c31c79e6a89e5e57e4507a5f63c8621',
+  },
+};
