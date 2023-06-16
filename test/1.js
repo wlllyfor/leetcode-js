1c89ceff326c98cd21dd055befd8f6bc04cc101b
wlllyfor <958167224@qq.com>
2023-06-16 17:03:48 +0800

    feat: find all nodes

--- test/1.js
+++ test/1.js
@@ -1,211 +1,152 @@
-const a = {
-  Response: {
-    Data: {
-      root: {
-        ID: -1,
-        Pid: -1,
-        Name: 'root',
-        Remark: '根节点',
+const tree = [
+  {
+    value: 1,
+    children: [
+      {
+        value: 3,
         children: [
           {
-            ID: 1,
-            Pid: -1,
-            Name: '公有云-供测试',
-            Remark: '私有云目录类型test1047',
-            State: 1,
+            value: 6,
             children: [
               {
-                ID: 6,
-                Pid: 1,
-                Name: '服务诉求',
-                Remark: '服务诉求',
-                State: 1,
-                children: [
-                  {
-                    ID: 7,
-                    Pid: 6,
-                    Name: '专家服务',
-                    Remark: '专家服务',
-                    State: 1,
-                    children: [
-                      { ID: 21, Pid: 7, Name: '高可用建设', State: 1 },
-                    ],
-                  },
-                  {
-                    ID: 8,
-                    Pid: 6,
-                    Name: '三级目录2',
-                    Remark: '重复性测试',
-                    State: 1,
-                    children: [
-                      { ID: 22, Pid: 8, Name: '子目录345435', State: 1 },
-                      { ID: 43, Pid: 8, Name: '子目录', State: 1 },
-                    ],
-                  },
-                  {
-                    ID: 10,
-                    Pid: 6,
-                    Name: '高级服务',
-                    Remark: '高级服务类型',
-                    State: 1,
-                    children: [{ ID: 41, Pid: 10, Name: '赋能交流', State: 1 }],
-                  },
-                ],
+                value: 8,
+              },
+              {
+                value: 9,
               },
             ],
           },
           {
-            ID: 3,
-            Pid: -1,
-            Name: '公有云233',
-            Remark: '公有云目录类型',
-            State: 1,
+            value: 7,
             children: [
               {
-                ID: 62,
-                Pid: 3,
-                Name: '服务诉求',
-                Remark: '服务诉求',
-                State: 1,
-                children: [
-                  {
-                    ID: 63,
-                    Pid: 62,
-                    Name: '专家服务',
-                    Remark: '专家服务',
-                    State: 1,
-                    children: [
-                      {
-                        ID: 64,
-                        Pid: 63,
-                        Name: '架构优化',
-                        Remark: '架构优化',
-                        State: 1,
-                      },
-                      {
-                        ID: 65,
-                        Pid: 63,
-                        Name: '稳定性治理',
-                        Remark: '稳定性治理',
-                        State: 1,
-                      },
-                      {
-                        ID: 66,
-                        Pid: 63,
-                        Name: '高可用建设',
-                        Remark: '高可用建设',
-                        State: 1,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            ID: 11,
-            Pid: -1,
-            Name: '其他诉求',
-            Remark: '其他诉求类型',
-            State: 1,
-            children: [
-              {
-                ID: 12,
-                Pid: 11,
-                Name: '商务相关',
-                Remark: '商务相关类型',
-                State: 1,
-                children: [
-                  {
-                    ID: 13,
-                    Pid: 12,
-                    Name: '投诉建议',
-                    Remark: '投诉建议类型111',
-                    State: 1,
-                    children: [
-                      {
-                        ID: 14,
-                        Pid: 13,
-                        Name: '服务质量投诉',
-                        Remark: '服务质量投诉类型',
-                        State: 1,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            ID: 17,
-            Pid: -1,
-            Name: '私有云123',
-            Remark: '私有云目录类型',
-            State: 1,
-            children: [
-              {
-                ID: 24,
-                Pid: 17,
-                Name: '私有云111-第二级234234234',
-                Remark: '私有云目录类型',
-                State: 1,
-                children: [
-                  {
-                    ID: 25,
-                    Pid: 24,
-                    Name: '私有云111-第三级',
-                    Remark: '私有云目录类型',
-                    State: 1,
-                    children: [
-                      {
-                        ID: 26,
-                        Pid: 25,
-                        Name: '私有云111-第四级',
-                        Remark: '私有云目录类型',
-                        State: 1,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            ID: 47,
-            Pid: -1,
-            Name: '私有云测试',
-            State: 1,
-            children: [
-              {
-                ID: 50,
-                Pid: 47,
-                Name: '私有云二级目录s2',
-                State: 1,
-                children: [
-                  {
-                    ID: 51,
-                    Pid: 50,
-                    Name: '私有云三级目录t1',
-                    State: 1,
-                    children: [
-                      { ID: 55, Pid: 51, Name: '私有云四级目录f1', State: 1 },
-                      {
-                        ID: 56,
-                        Pid: 51,
-                        Name: '私有云四级目录f2',
-                        Remark: 'cesdsssssss',
-                        State: 1,
-                      },
-                    ],
-                  },
-                ],
+                value: 10,
               },
             ],
           },
         ],
       },
-    },
-    Status: 0,
-    RequestId: '6c31c79e6a89e5e57e4507a5f63c8621',
+      {
+        value: 4,
+      },
+      {
+        value: 5,
+      },
+    ],
   },
-};
+  {
+    value: 2,
+  },
+];
+
+function findAllPath(tree, target) { 
+
+  // 给树的每个节点加一个层级属性
+  function addLevel(tree, level = 1) {
+    tree.forEach((node) => {
+      node.level = level;
+      if (node.children) {
+        addLevel(node.children, level + 1);
+      }
+    });
+  }
+
+  addLevel(tree)
+
+  // 获取某个节点从根节点到这个节点的路径
+  function getPath(tree, value) {
+    const path = [];
+    function findPath(tree, value) {
+      for (let i = 0; i < tree.length; i++) {
+        const node = tree[i];
+        if (node.value === value) {
+          path.push(node);
+          return true;
+        }
+        if (node.children) {
+          path.push(node);
+          if (findPath(node.children, value)) {
+            return true;
+          }
+          path.pop();
+        }
+      }
+      return false;
+    }
+    findPath(tree, value);
+    return path;
+  }
+
+  const path = getPath(tree, target);
+
+  // 获取某个节点的所有子节点，包括子节点的子节点，一直到叶子节点
+  function getChildren(tree, value) {
+    const children = [];
+    function findChildren(tree, value) {
+      for (let i = 0; i < tree.length; i++) {
+        const node = tree[i];
+        if (node.value === value) {
+          if (node.children) {
+            node.children.forEach(item => {
+              children.push(item)
+              findChildren(node.children, item.value)
+            })
+          }
+          return true;
+        }
+        if (node.children) {
+          if (findChildren(node.children, value)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+    findChildren(tree, value);
+    return children;
+  }
+
+  const children = getChildren(tree, target)
+
+  console.log('path.concat(children)', path.concat(children));
+
+  return path.concat(children)
+}
+
+function batchFindAllPath(tree, ids) { 
+  const levels = [
+    new Set(),
+    new Set(),
+    new Set(),
+    new Set(),
+  ];
+  ids.forEach(id => { 
+    const path = findAllPath(tree, id);
+    path.forEach(item => { 
+      if (item.level === 1) {
+        levels[0].add(item.value)
+      } else if (item.level === 2) {
+        levels[1].add(item.value)
+      } else if (item.level === 3) {
+        levels[2].add(item.value)
+      } else if (item.level === 4) {
+        levels[3].add(item.value)
+      }
+    })
+  })
+
+  return [
+    Array.from(levels[0]),
+    Array.from(levels[1]),
+    Array.from(levels[2]),
+    Array.from(levels[3]),
+  ]
+}
+
+console.log(batchFindAllPath(tree, [8, 9]));
+
+
+
+
+
+
