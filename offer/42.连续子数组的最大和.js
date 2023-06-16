387ad64ee6a671fd5af6ef0c29ae4de3a55913ee
wlllyfor <958167224@qq.com>
2023-06-16 17:13:50 +0800

    feat: add 42

---
+++ offer/42.连续子数组的最大和.js
@@ -0,0 +1,12 @@
+function maxSubArray (nums) {
+  let maxSum = nums[0];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    maxSum = Math.max(maxSum, sum);
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return maxSum;
+};
