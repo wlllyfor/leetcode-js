6bbce478f11fdf66c59bcb2326c331a21765215d
wlllyfor <958167224@qq.com>
2023-06-26 10:15:52 +0800

    feat: update 47

--- offer/47.礼物的价值.js
+++ offer/47.礼物的价值.js
@@ -26,3 +26,30 @@ function maxValue(grid) {
 
   return dp[rows-1][cols-1];
 }
+
+function maxValue(grid) {
+  if (!grid || grid.length === 0 || grid[0].length === 0) {
+    return 0;
+  }
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const dp = new Array(cols).fill(0);
+
+  dp[0] = grid[0][0];
+
+  // 初始化第一行
+  for (let j = 1; j < cols; j++) {
+    dp[j] = dp[j-1] + grid[0][j];
+  }
+
+  // 逐行计算最大价值
+  for (let i = 1; i < rows; i++) {
+    dp[0] += grid[i][0]; // 更新第一列的最大价值
+    for (let j = 1; j < cols; j++) {
+      dp[j] = Math.max(dp[j], dp[j-1]) + grid[i][j];
+    }
+  }
+
+  return dp[cols-1];
+}
