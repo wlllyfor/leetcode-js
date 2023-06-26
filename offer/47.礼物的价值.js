877c7dd5a84861953ffa749c9a3d5e4c766f9a14
wlllyfor <958167224@qq.com>
2023-06-26 10:13:27 +0800

    feat: add 47

---
+++ offer/47.礼物的价值.js
@@ -0,0 +1,28 @@
+function maxValue(grid) {
+  if (!grid || grid.length === 0 || grid[0].length === 0) {
+    return 0;
+  }
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const dp = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+
+  dp[0][0] = grid[0][0];
+
+  // 初始化第一行和第一列
+  for (let i = 1; i < rows; i++) {
+    dp[i][0] = dp[i-1][0] + grid[i][0];
+  }
+  for (let j = 1; j < cols; j++) {
+    dp[0][j] = dp[0][j-1] + grid[0][j];
+  }
+
+  // 逐行逐列计算最大价值
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+    }
+  }
+
+  return dp[rows-1][cols-1];
+}
