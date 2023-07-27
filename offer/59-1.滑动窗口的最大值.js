084b6d87b1a8b2915c5d02494e6bb8129a0503a1
wlllyfor <958167224@qq.com>
2023-07-27 22:14:24 +0800

    feat: add 59-1

---
+++ offer/59-1.滑动窗口的最大值.js
@@ -0,0 +1,22 @@
+function maxSlidingWindow(nums, k) {
+  const result = [];
+  const deque = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    while (deque.length > 0 && deque[0] < i - k + 1) {
+      deque.shift();
+    }
+
+    while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+
+    deque.push(i);
+
+    if (i >= k - 1) {
+      result.push(nums[deque[0]]);
+    }
+  }
+
+  return result;
+}
