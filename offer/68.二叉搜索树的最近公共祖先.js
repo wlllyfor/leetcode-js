51678eecf7584b47fa1d790ed9f27970bb51ca32
chriswlwang <chriswlwang@tencent.com>
2023-08-16 10:03:42 +0800

    feat: add 68

---
+++ offer/68.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,12 @@
+function lowestCommonAncestor(root, p, q) {
+  while (root) {
+    if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else {
+      break;
+    }
+  }
+  return root;
+}
