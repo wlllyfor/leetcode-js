e68cb039f5dcfaac857f12d1a078f78b8527b3ee
wlllyfor <958167224@qq.com>
2023-08-17 11:36:19 +0800

    feat: add 69

---
+++ offer/69.二叉树的最近公共祖先.js
@@ -0,0 +1,18 @@
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) {
+    return root;
+  }
+  
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  
+  if (!left) {
+    return right;
+  }
+  
+  if (!right) {
+    return left;
+  }
+  
+  return root;
+}
