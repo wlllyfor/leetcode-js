9c5a0879ba34305f470061bb767a7f09e66414d0
wlllyfor <958167224@qq.com>
2023-09-06 15:57:08 +0800

    feat: update 7

--- go/7.if.go
+++ go/7.if.go
@@ -115,33 +115,68 @@
 // 	}
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+
+// 	c1 := make(chan string)
+// 	c2 := make(chan string)
+
+// 	go func() {
+// 		time.Sleep(1 * time.Second)
+// 		c1 <- "one"
+// 	}()
+// 	go func() {
+// 		time.Sleep(2 * time.Second)
+// 		c2 <- "two"
+// 	}()
+
+// 	for i := 0; i < 2; i++ {
+// 		select {
+// 		case msg1 := <-c1:
+// 			fmt.Println("received", msg1)
+// 		case msg2 := <-c2:
+// 			fmt.Println("received", msg2)
+// 		}
+// 	}
+// }
+
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
+	// 定义两个通道
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
-	c1 := make(chan string)
-	c2 := make(chan string)
-
+	// 启动两个 goroutine，分别从两个通道中获取数据
 	go func() {
-		time.Sleep(1 * time.Second)
-		c1 <- "one"
+		for {
+			ch1 <- "from 1"
+		}
 	}()
 	go func() {
-		time.Sleep(2 * time.Second)
-		c2 <- "two"
+		for {
+			ch2 <- "from 2"
+		}
 	}()
 
-	for i := 0; i < 2; i++ {
+	// 使用 select 语句非阻塞地从两个通道中获取数据
+	for {
 		select {
-		case msg1 := <-c1:
-			fmt.Println("received", msg1)
-		case msg2 := <-c2:
-			fmt.Println("received", msg2)
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		default:
+			// 如果两个通道都没有可用的数据，则执行这里的语句
+			fmt.Println("no message received")
 		}
 	}
 }
