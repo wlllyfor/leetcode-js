1767b5fd32289b50c4886636f963891dcc276d8a
wlllyfor <958167224@qq.com>
2023-10-08 11:26:04 +0800

    feat: add 20

--- go/20.map.go
+++ go/20.map.go
@@ -1,29 +1,56 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var siteMap map[string]string /*创建集合 */
+// 	siteMap = make(map[string]string)
+
+// 	/* map 插入 key - value 对,各个国家对应的首都 */
+// 	siteMap["Google"] = "谷歌"
+// 	siteMap["Runoob"] = "菜鸟教程"
+// 	siteMap["Baidu"] = "百度"
+// 	siteMap["Wiki"] = "维基百科"
+
+// 	/*使用键输出地图值 */
+// 	for site := range siteMap {
+// 		fmt.Println(site, "首都是", siteMap[site])
+// 	}
+
+// 	/*查看元素在集合中是否存在 */
+// 	name, ok := siteMap["Facebook"] /*如果确定是真实的,则存在,否则不存在 */
+// 	/*fmt.Println(capital) */
+// 	/*fmt.Println(ok) */
+// 	if ok {
+// 		fmt.Println("Facebook 的 站点是", name)
+// 	} else {
+// 		fmt.Println("Facebook 站点不存在")
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	var siteMap map[string]string /*创建集合 */
-	siteMap = make(map[string]string)
+	/* 创建map */
+	countryCapitalMap := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo", "India": "New delhi"}
 
-	/* map 插入 key - value 对,各个国家对应的首都 */
-	siteMap["Google"] = "谷歌"
-	siteMap["Runoob"] = "菜鸟教程"
-	siteMap["Baidu"] = "百度"
-	siteMap["Wiki"] = "维基百科"
+	fmt.Println("原始地图")
 
-	/*使用键输出地图值 */
-	for site := range siteMap {
-		fmt.Println(site, "首都是", siteMap[site])
+	/* 打印地图 */
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
 	}
 
-	/*查看元素在集合中是否存在 */
-	name, ok := siteMap["Facebook"] /*如果确定是真实的,则存在,否则不存在 */
-	/*fmt.Println(capital) */
-	/*fmt.Println(ok) */
-	if ok {
-		fmt.Println("Facebook 的 站点是", name)
-	} else {
-		fmt.Println("Facebook 站点不存在")
+	/*删除元素*/
+	delete(countryCapitalMap, "France")
+	fmt.Println("法国条目被删除")
+
+	fmt.Println("删除元素后地图")
+
+	/*打印地图*/
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
 	}
 }
--- test/1.js
+++ test/1.js
@@ -1,52 +1,7 @@
-function fillMissingData(arr) {
-  const result = [];
-  const startDate = new Date(arr[0].time);
-  const endDate = new Date(startDate.getTime() + (180 * 24 * 60 * 60 * 1000)); // 180天后的日期
-
-  let currentDate = startDate;
-  let currentIndex = 0;
-
-  while (currentDate <= endDate) {
-    const currentDateString = currentDate.toISOString().split('T')[0];
-
-    if (arr[currentIndex] && arr[currentIndex].time === currentDateString) {
-      result.push(arr[currentIndex]);
-      currentIndex++;
-    } else {
-      result.push({
-        time: currentDateString,
-        count: 0,
-      });
-    }
-
-    currentDate.setDate(currentDate.getDate() + 1);
-  }
-
-  return result;
-}
-
 const arr = [
-  {
-    "time": "2023-03-10",
-    "count": 5
-  },
-  {
-    "time": "2023-04-12",
-    "count": 4
-  },
-  {
-    "time": "2023-05-12",
-    "count": 11
-  },
-  {
-    "time": "2023-06-13",
-    "count": 1
-  },
-  {
-    "time": "2023-07-14",
-    "count": 1
-  },
-];
+  
+]
+ 
+const arr1 = arr.map(item => item.id).filter(item => item.id)
 
-const filledData = fillMissingData(arr);
-console.log(filledData);
+console.log('arr1', arr1);
