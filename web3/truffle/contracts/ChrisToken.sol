776370c56b3f29ede02baa9810f7179e716954d0
wlllyfor <958167224@qq.com>
2024-01-02 19:57:21 +0800

    feat: approve

--- web3/truffle/contracts/ChrisToken.sol
+++ web3/truffle/contracts/ChrisToken.sol
@@ -12,6 +12,7 @@ contract ChrisToken {
   uint256 public totalSupply;
 
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor () {
     totalSupply = 1000000 * (10 ** decimals);
@@ -19,6 +20,7 @@ contract ChrisToken {
   }
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
     require(_to != address(0));
@@ -32,5 +34,11 @@ contract ChrisToken {
     balanceOf[_to] = balanceOf[_to].add(_value);
 
     emit Transfer(_from, _to, _value);
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    require(_spender != address(0));
+    allowance[msg.sender][_spender] = _value;
+    return true;
   }
 }
