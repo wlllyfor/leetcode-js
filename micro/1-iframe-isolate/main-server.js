64b7e8f0e4570518fbdb57eb4742a9c469db25e3
wlllyfor <958167224@qq.com>
2024-06-18 09:29:02 +0800

    feat: iframe isolate

---
+++ micro/1-iframe-isolate/main-server.js
@@ -0,0 +1,29 @@
+ // main-server.js
+ import path from 'path';
+ // https://github.com/expressjs/express
+ import express from 'express';
+ // ejs 中文网站: https://ejs.bootcss.com/#promo
+ // ejs express 示例: https://github.com/expressjs/express/blob/master/examples/ejs/index.js
+ import ejs from "ejs";
+ import config from './config.js';
+ const { port, host, __dirname } = config;
+ 
+ const app = express();
+ 
+ app.engine(".html", ejs.__express);
+ app.set("views", path.join(__dirname, "views"));
+ app.set("view engine", "html");
+ 
+ // 浏览器访问 http://${host}:${port.main}/ 时会渲染 views/main.html 
+ app.get("/", function (req, res) {
+   // 使用 ejs 模版引擎填充主应用 views/main.html 中的 iframeUrl 变量，并将其渲染到浏览器
+   res.render("main", {
+     // 填充 iframe 应用的地址，只有端口不同，iframe 应用和 main 应用跨域但是同站
+     iframeUrl: `http://${host}:${port.micro}`
+   });
+ });
+ 
+ // 启动 Node 服务
+ app.listen(port.main, host);
+console.log(`server start at http://${host}:${port.main}/`);
+ 
